2df3e73e7ffaff834cf971bd9b8dba9dfa4ef8a4
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-03 22:41:31 +0100

    show usage of obstacle_img2dist_img

--- main_image.py
+++ main_image.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from GridWorld.random_obstacles import create_rectangle_image, create_perlin_image
-
+from GridWorld.obstacle_distance import obstacle_img2dist_img
 
 # Block World
 n_voxels = (64, 64)
@@ -18,9 +18,11 @@ plt.show()
 res = 4
 threshold = 0.5
 img = create_perlin_image(n_voxels=n_voxels, res=res, threshold=threshold)
-
+plt.figure()
 plt.imshow(img, origin='lower')
 plt.show()
 
-
-
+dist_img = obstacle_img2dist_img(img=img, voxel_size=1/64, )
+plt.figure()
+plt.imshow(dist_img, origin='lower')
+plt.show()
